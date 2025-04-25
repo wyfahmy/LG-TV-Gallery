dfa743f70957f42985a5a70d01ee9b0bab06fa05
wyfahmy <wy.fahmy@gmail.com>
2025-04-25 21:45:58 +0300

    fix(images path): change images path
    added the correct path to images instead of "image1, image2, image3, etc.."

--- settings.js
+++ settings.js
@@ -6,8 +6,6 @@ const SlideshowSettings = {
   randomizeOrder: true,
   objectFitMode: "cover",
   imageList: [
-    "images/image1.jpg",
-    "images/image2.jpg",
-    "images/image3.jpg"
+    "images/wallhaven-z8glwy.jpg",    "images/wallhaven-v93ml8.jpg",    "images/wallhaven-gpwqw3.jpg",    "images/wallhaven-76r1vy.jpg",    "images/wallhaven-9m8qzd.jpg",    "images/wallhaven-8opg6o.jpg",    "images/wallhaven-5wkg29.jpg"
   ]
 };
